edf455a03027d6b2338e62afc67496aa3a68a56f
npnono <97909866+npnono@users.noreply.github.com>
2026-04-06 15:36:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+function scrollToSection(id) {
+  document.getElementById(id).scrollIntoView({
+    behavior: "smooth"
+  });
+}
+
 function toggleMenu(){
   document.getElementById("navLinks").classList.toggle("active");
 }
